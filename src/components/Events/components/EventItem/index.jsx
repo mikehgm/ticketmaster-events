c282cd493b57d42ebb953b3d6ab4da4f77e5eecb
MGuzman <mguzman.pc@gmail.com>
2025-03-21 11:39:43 -0700

    practica react clase 42 - usando styles con modules

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,4 +1,4 @@
-import './styles.css';
+import styles from './EventItem.module.css';
 
 const EventItem = ({ info, name, image, id, onEventClick }) => {
     const handleSeeMoreClick = (evt) => {
@@ -7,12 +7,12 @@ const EventItem = ({ info, name, image, id, onEventClick }) => {
     }
 
     return (
-        <div className='event-item-container'>
+        <div className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
             <img src={image} alt="image-event" width={200} height={200}/>
-            <div className='event-info-container'>
-                <h4 className='event-name'>{name}</h4>
-                <p className='event-info'>{info}</p>
-                <button onClick={handleSeeMoreClick} className='see-more-btn'>Ver mas</button>
+            <div className={styles.eventInfoContainer}>
+                <h4 className={styles.eventName}>{name}</h4>
+                <p className={styles.eventInfo}>{info}</p>
+                <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>Ver mas</button>
             </div>
         </div>
     );
