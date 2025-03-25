aed88a3108b57a22d93425c782896ad21f4fd93f
MGuzman <mguzman.pc@gmail.com>
2025-03-25 12:52:32 -0700

    practica react - agregadon prevEvents

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const useEventsData = () => {
     const [ data, setData ] = useState([]);
     const [ isLoading, setIsLoading ] = useState(true);
     const [ error, setError ] = useState();
+    const apiKey = "raf3bEA4UqgxUSC7mwHoWXRIPC7OtgSe";
 
 
-    const fetchEvents = async (params) => {
+    const fetchEvents = useCallback(async (params) => {
+        console.log("Fetching events...");  // Debugging log
+        setIsLoading(true);
+        setError(null);
         try {
             if (params && params !== '' && params.length > 0) {
                 params = `&keyword=${params}`;
@@ -14,15 +18,17 @@ const useEventsData = () => {
                 params = '';
             }
 
-            const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=raf3bEA4UqgxUSC7mwHoWXRIPC7OtgSe&countryCode=MX${params?.length ? params : ''}`);
+            const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&countryCode=MX${params?.length ? params : ''}`);
             const data = await response.json();
-            setData(data);
+
+            setData(prevEvents => data._embedded?.events || prevEvents); // Prevents unnecessary re-renders
+
         } catch (error) {
             setError(error);
         } finally {
             setIsLoading(false);
         }
-    }
+    }, [apiKey]);
 
     console.log('events', data);
 
